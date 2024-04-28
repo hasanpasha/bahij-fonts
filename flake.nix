6a8a3e02585dda85c4f7f58a110e78c9976857d2
hasanpasha <bashahsn22@gmail.com>
2024-04-28 10:33:56 +0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description =
+    "A flake giving access to fonts that I use, outside of nixpkgs.";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  # inputs.bahij = "./bahij";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+        installInstructions = ''
+          mkdir -p $out/share/fonts/freetype
+          cp $src/*/*.ttf $out/share/fonts/freetype
+        '';
+      in {
+        packages.default = pkgs.stdenvNoCC.mkDerivation {
+          name = "bahij-fonts-0.0.1";
+          src = ./bahij;
+          installPhase = installInstructions;
+          meta = { description = "A Bahij Fonts Family derivation."; };
+        };
+      });
+}
